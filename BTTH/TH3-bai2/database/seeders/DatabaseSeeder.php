d0befab4d635616d3e2b7e51289be1d97ecb9e4f
Khoa <Khoalon89@gmail.com>
2024-12-09 13:14:10 +0700

    update

--- BTTH/TH3-bai2/database/seeders/DatabaseSeeder.php
+++ BTTH/TH3-bai2/database/seeders/DatabaseSeeder.php
@@ -2,22 +2,20 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ClassesTableSeeder;  // Import ClassesTableSeeder
+use Database\Seeders\StudentsTableSeeder; // Import StudentsTableSeeder
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            ClassesTableSeeder::class,  // Gọi Seeder cho bảng classes
+            StudentsTableSeeder::class, // Gọi Seeder cho bảng students
         ]);
     }
 }
