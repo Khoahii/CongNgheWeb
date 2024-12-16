3089e0c3845108d42f839a48fefae43ac7323ad0
Khoa <Khoalon89@gmail.com>
2024-12-16 10:33:47 +0700

    model

--- BTTH/TH4/app/Models/computer.php
+++ BTTH/TH4/app/Models/computer.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class computer extends Model
+class Computer extends Model
 {
-    //
+  use HasFactory;
+
+  // Định nghĩa tên bảng nếu không theo tên mặc định (số nhiều của tên model)
+  protected $table = 'computers';
+
+  // Các trường có thể gán giá trị
+  protected $fillable = [
+    'computer_name',
+    'model',
+    'operating_system',
+    'processor',
+    'memory',
+    'available'
+  ];
+
+  // Định nghĩa quan hệ với bảng Issues (một máy tính có thể có nhiều vấn đề)
+  public function issues()
+  {
+    return $this->hasMany(Issue::class);
+  }
 }
